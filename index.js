98b6d442513f53676fb18f56c4dde3a822e9ce41
mkoenke <mkoenke@sas.upenn.edu>
2020-11-18 19:08:00 -0500

    commit tree 3b360715af6639a2456cc9feb99fe005e8e386e0

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W")
+  if (result){
+    return result.year
+  } else return undefined
+}
